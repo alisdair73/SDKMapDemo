a03d3eda8454be4528636ec810debb7f5964fb08
Alisdair Templeton <alisdair73@gmail.com>
2019-12-29 22:39:19 +1100

    Initial Commit

---
+++ SDKMapDemo/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  SDKMapDemo
+//
+//  Created by Alisdair Templeton on 29/12/19.
+//  Copyright © 2019 Alisdair Templeton. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
